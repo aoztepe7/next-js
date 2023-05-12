7ce6ca373f736a0b93a103b7e428ce035077a6e4
Ali Oztepe <ali.oztepe@santsg.com>
2023-05-12 16:47:28 +0300

    sendgrid

--- app/[language]/page.tsx
+++ app/[language]/page.tsx
@@ -1,7 +1,4 @@
-import WelcomeEmail from "@/emails/WelcomeTemplate";
 import { ValidLocale, getTranslator } from "@/i18n";
-import { sendEmail } from "@/lib/email";
-import { render } from "@react-email/render";
 
 export default async function Home({
   params,
@@ -12,10 +9,5 @@ export default async function Home({
     `${params.language}` as ValidLocale // our middleware ensures this is valid
   );
 
-  await sendEmail({
-    to: "oztepeali@gmail.com",
-    subject: "test",
-    html: render(WelcomeEmail())
-  })
   return <h1>{translate("welcome.helloWorld")}</h1>;
 }
--- app/[language]/products/page.tsx
+++ app/[language]/products/page.tsx
@@ -1,8 +1,29 @@
-import { Fragment } from "react";
+import sendgrid from "@sendgrid/mail";
+
 
 async function Products() {
+    const { SG_API_KEY, FROM_MAIL, TO_MAIL } = process.env;
+    if (process.env.SG_API_KEY) {
+        sendgrid.setApiKey(process.env.SG_API_KEY);
+        console.log("setted api key");
+        if (process.env.TO_MAIL && process.env.FROM_MAIL) {
+            console.log("deeper");
+            try {
+                await sendgrid.send({
+                    to: process.env.TO_MAIL,
+                    from: process.env.FROM_MAIL, // your website email address here
+                    subject: `Test`,
+                    html: `<div>You've got a mail</div>`,
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+    }
     return (
         <>
+            <span> Mail Send </span>
         </>
     )
 }
